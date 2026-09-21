1fd20c521fd845b37a487d28c9127c01432084d5
bajissah-unt <bajissah@gmail.com>
2026-09-21 15:53:27 +0000

    manually analysing data

--- spending_analyser.py
+++ spending_analyser.py
@@ -1,1 +1,25 @@
-print ("hello")
+transactions = [
+    {"merchant" : "walmart", "category": "groceries", "amount": 120},
+    {"merchant" : "Lavanderia", "category": "food", "amount": 10.32},
+    {"merchant" : "shell", "category": "gas", "amount": 160},
+    {"merchant" : "TotalWireless", "category": "phoneplan", "amount": 160}
+
+]
+#count the total amount
+count = 0
+for trans in transactions:
+    count = count + trans["amount"]
+print (f"The total transactions so far : ${count:.2f}")
+highest_amount = 0
+
+#calculate max and category
+for trans in transactions:
+    if trans["amount"]>highest_amount:
+        highest_amount = trans["amount"]
+        highest_merchant = trans["merchant"]
+
+print (f"The highest money spent was ${highest_amount:.2f} at {highest_merchant}")
+
+#calculate average
+print (f"The average expenditure is {count/len(transactions):.2f}")
+
